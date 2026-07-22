83bacd7f103a2abc94fe52fa80aec8c164d702ed
Маркіян О <ostasmarkian@gmail.com>
2026-07-22 21:54:48 +0300

    add monitoring which updates infomation every 10 minutes

--- website monitoring/monitoring.py
+++ website monitoring/monitoring.py
@@ -2,37 +2,73 @@ import time
 from config import DATABASE_CONFIG
 import psycopg
 import requests
+import telebot
+from config import TG_BOT_TOKEN, CHAT_ID
+
+bot = telebot.TeleBot(TG_BOT_TOKEN)
 
 
 def update():
     with psycopg.connect(**DATABASE_CONFIG) as connection:
         with connection.cursor() as cursor:
             cursor.execute("""
-                           SELECT websites.url,checks.status_code,website.id FROM checks
-                           JOIN websites
-                           ON checks.website_id = websites.id
+                            SELECT DISTINCT ON (websites.id)
+                                websites.url,
+                                checks.status_code,
+                                websites.id
+                            FROM websites
+                            JOIN checks
+                                ON checks.website_id = websites.id
+                            ORDER BY
+                                websites.id,
+                                checks.checked_at DESC,
+                                checks.id DESC;
                            """)
             result = cursor.fetchall()
             for row in result:
                 status = row["status_code"]
                 url = row["url"]
-                start = time.perf_counter()
-                rest = requests.get(url, timeout=5)
-                status_n = rest.status_code()
                 website_id = row["id"]
-                response_time = time.perf_counter() - start
-                if status != status_n:
-                    print("Сайт змінив статус")
-    with psycopg.connect(**DATABASE_CONFIG) as connection:
-        with connection.cursor() as cursor:
-            cursor.execute(
-                """
-                        INSERT INTO checks(website_id,status_code,response_time,is_available,error_message)
-                        VALUES(%s,%s,%s,%s,%s)
-                           """,
-                (
-                    website_id,
-                    status_n,
-                    response_time,
-                ),
-            )
+                status_code = None
+                response_time = None
+                is_available = False
+                error_message = None
+                start = time.perf_counter()
+                try:
+                    check = requests.get(url, timeout=5)
+                    status_code = check.status_code
+                    check.raise_for_status()
+                    response_time = time.perf_counter() - start
+                    is_available = True
+                    error_message = None
+                except requests.HTTPError as error:
+                    response_time = time.perf_counter() - start
+                    error_message = str(error)[:64]
+                    is_available = False
+
+                except requests.RequestException as error:
+                    response_time = time.perf_counter() - start
+                    status_code = None
+                    is_available = False
+                    error_message = str(error)[:64]
+                response_time = round(response_time, 3)
+                if status != status_code:
+                    bot.send_message(CHAT_ID, f"{url} change status to {status_code}")
+                cursor.execute(
+                    """
+                            INSERT INTO checks(website_id,status_code,response_time,is_available,error_message)
+                            VALUES(%s,%s,%s,%s,%s)
+                            """,
+                    (
+                        website_id,
+                        status_code,
+                        response_time,
+                        is_available,
+                        error_message,
+                    ),
+                )
+
+
+while True:
+    update()
+    time.sleep(600)
