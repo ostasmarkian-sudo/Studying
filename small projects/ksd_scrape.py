b2e3c079e8ac53475c1e337c38341d205eb57978
Маркіян О <ostasmarkian@gmail.com>
2026-07-29 18:27:20 +0300

    developer a web scraper from a book site

---
+++ small projects/ksd_scrape.py
@@ -0,0 +1,118 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import psycopg
+from config import DATABASE_CONFIG
+
+headers = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/150.0.0.0 Safari/537.36"
+    ),
+    "Accept": (
+        "text/html,application/xhtml+xml,application/xml;"
+        "q=0.9,image/avif,image/webp,*/*;q=0.8"
+    ),
+    "Accept-Language": "uk-UA,uk;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Referer": "https://ksd.ua/",
+}
+book = "ui-catalog-card--variant-default mui-1i20r6w-ui-catalog-card"
+page = 1
+id = 1
+with psycopg.connect(**DATABASE_CONFIG) as connection:
+    with connection.cursor() as cursor:
+        while page < 233:
+            url = f"https://ksd.ua/books/special/all/page-{page}"
+            req = requests.get(url, timeout=10, headers=headers)
+            req.raise_for_status()
+            soup = BeautifulSoup(req.text, "html.parser")
+            for match in soup.find_all("a", class_=book):
+                id += 1
+                book_name = match.find(
+                    "p",
+                    class_="MuiTypography-root MuiTypography-body1 ui-catalog-card__title mui-1st0ryw",
+                )
+                book_name = book_name.text
+                book_author = match.find(
+                    "p",
+                    class_="MuiTypography-root MuiTypography-body2 ui-catalog-card__authors mui-y7plz2",
+                )
+                try:
+                    book_author = book_author.text
+                except AttributeError:
+                    book_author = "немає"
+                book_rate = match.find(
+                    "div",
+                    class_="ui-catalog-card__rating-wrapper",
+                )
+                try:
+                    available = True
+                    prise = match.find("div", class_="MuiBox-root mui-0")
+                    book_price = prise.find(
+                        "span",
+                        class_="MuiTypography-root MuiTypography-h4 ui-catalog-card__price mui-1ng6fsq",
+                    ).span.text
+                    ds = prise.find(
+                        "span",
+                        class_="MuiTypography-root MuiTypography-subtitle3 ui-catalog-card__price ui-catalog-card__price--old mui-161am5x",
+                    )
+                    if ds == None:
+                        discount = 0
+                    else:
+                        ds = ds.text
+                        d = re.search(r"(\d+)", ds)
+                        ds = d.group(1)
+                        discount = int(ds) - int(book_price)
+                except AttributeError:
+                    book_price = 0
+                    discount = 0
+                    available = False
+                rating_paragraphs = book_rate.select("p")
+                rate = rating_paragraphs[0].get_text(strip=True)
+                if rate == "":
+                    rate = None
+                rate_count = rating_paragraphs[1].get_text(strip=True)
+                if rate_count == "":
+                    rate_count = 0
+                d = re.search(r"(\d+)", rate_count)
+                rate_count = d.group(1)
+                product_link = match.get("href")
+                product_link = "ksd.ua" + product_link
+                cursor.execute(
+                    """
+                    INSERT INTO books (
+                        book_name,
+                        book_author,
+                        book_rate,
+                        book_count_rate,
+                        book_price,
+                        bpwd,
+                        product_link,
+                        available
+                    )
+                    VALUES (%s, %s, %s, %s, %s, %s, %s,%s)
+                    ON CONFLICT (product_link)
+                    DO UPDATE SET
+                        book_name = EXCLUDED.book_name,
+                        book_author = EXCLUDED.book_author,
+                        book_rate = EXCLUDED.book_rate,
+                        book_count_rate = EXCLUDED.book_count_rate,
+                        book_price = EXCLUDED.book_price,
+                        bpwd = EXCLUDED.bpwd,
+                        available = EXCLUDED.available;
+                    """,
+                    (
+                        book_name,
+                        book_author,
+                        rate,
+                        rate_count,
+                        book_price,
+                        discount,
+                        product_link,
+                        available,
+                    ),
+                )
+
+                print(f"Збережено: {book_name},сторінка: {page},книга: {id}")
+            page += 1
--- small projects/tdkfk.py
+++ small projects/tdkfk.py
@@ -2,19 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-url = "https://books.toscrape.com/"
+x = 1
+while x < 51:
+    url = f"https://books.toscrape.com/catalogue/page-{x}.html"
 
-response = requests.get(url, timeout=10)
-response.raise_for_status()
+    response = requests.get(url, timeout=10)
+    response.raise_for_status()
 
-soup = BeautifulSoup(response.text, "html.parser")
-for match in soup.find_all("li", class_="col-xs-6 col-sm-4 col-md-3 col-lg-3"):
-    product_pod = match.find("article", class_="product_pod")
-    print(product_pod.h3.a.text)
-    product_prise = product_pod.find("div", class_="product_price")
-    filt = re.search(r"(\d+)", product_prise.p.text)
-    print(filt.group(1) + "£")
-    instock_availability = product_prise.find(
-        "p", class_="instock availability"
-    ).text.strip()
-    print(instock_availability)
+    soup = BeautifulSoup(response.text, "html.parser")
+    for match in soup.find_all("li", class_="col-xs-6 col-sm-4 col-md-3 col-lg-3"):
+        product_pod = match.find("article", class_="product_pod")
+        print(product_pod.h3.a.text)
+        product_prise = product_pod.find("div", class_="product_price")
+        filt = re.search(r"(\d+)", product_prise.p.text)
+        print(filt.group(1) + "£")
+        instock_availability = product_prise.find(
+            "p", class_="instock availability"
+        ).text.strip()
+        print(instock_availability)
+    x += 1
